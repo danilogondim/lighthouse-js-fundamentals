1b6e32b75be6f72cbaf233e05bc3de0914765136
Danilo Gondim <daniloencarnacao@gmail.com>
2020-07-28 00:20:27 +0000

    Create a function that greets people

---
+++ hello.js
@@ -0,0 +1,18 @@
+//sayHello function
+//code 1
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+
+//--------------------------------------------
+//code 2
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Thiago");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
