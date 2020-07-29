d56d0c50f9eb65160419f74eae466163e0266b68
Danilo Gondim <daniloencarnacao@gmail.com>
2020-07-29 01:36:45 +0000

    Add a function that increases the garbage count depending on what trash is submitted

---
+++ smart-garbage.js
@@ -0,0 +1,14 @@
+// function that increases the garbage count depending on what trash is submitted.
+
+const smartGarbage = function (trash, bins) {
+  if (trash === 'waste') {
+    bins.waste++;
+  } else if (trash === 'recycling') {
+    bins.recycling++;
+  } else if (trash === 'compost') {
+    bins.compost++;
+  }
+  return bins
+}
+
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
